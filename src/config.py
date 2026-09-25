e8ef5454737f886ff8fe8c11d19f6177fb155581
maylasinha2755 <54087582+maylasinha2755@users.noreply.github.com>
2026-09-25 15:51:41 -0300

    Update config.py
    Configura variável de ambiente da API Gemini

--- src/config.py
+++ src/config.py
@@ -1,1 +1,8 @@
-# Configurações da aplicação OrganizaFin
+import os
+from dotenv import load_dotenv
+
+# Carrega as variáveis de ambiente
+load_dotenv()
+
+# Chave da API do Gemini
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
